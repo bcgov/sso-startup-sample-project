04ef95a9902a33b46407d879b2469ca157903b61
Ryan Jaeger <rjjaeger@amazon.com>
2020-07-03 10:21:59 -0700

    add budget and enhanced tagging support

--- terraform/aws/ecs.tf
+++ terraform/aws/ecs.tf
@@ -42,9 +42,11 @@ resource "aws_ecs_service" "main" {
   task_definition = aws_ecs_task_definition.app[count.index].arn
   desired_count   = var.client_app_count
   launch_type     = "FARGATE"
+  enable_ecs_managed_tags = true
+  propagate_tags = "TASK_DEFINITION"
   health_check_grace_period_seconds = 60
 
-	network_configuration {
+  network_configuration {
     security_groups  = [aws_security_group.ecs_tasks.id]
     subnets          = aws_subnet.private.*.id
     assign_public_ip = false
@@ -57,4 +59,6 @@ resource "aws_ecs_service" "main" {
   }
 
   depends_on = [aws_alb_listener.front_end, aws_iam_role_policy_attachment.ecs_task_execution_role]
+
+  tags = local.common_tags
 }
